15e8208038715b1dbdcc54dbd6da044df64de7bf
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2019-12-01 22:27:29 +0900

    modify gulp run gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,21 @@
+var config = require('./gulp/config.js');
 var gulp = require('gulp');
 var load = require('require-dir');
 
 load('./gulp/tasks', { recurse: true });
-gulp.task('default', () => {
-  console.log(process.env.NODE_ENV);
-});
+
+var development = [
+  'copy-third_party',
+  'copy-images',
+  'copy-javascripts',
+  'compile-sass'
+];
+
+var production = [
+  'copy-third_party',
+  'copy-images',
+  'minify-javascripts',
+  'compile-sass'
+];
+
+gulp.task('default', config.env.IS_DEVELOPMENT ? development : production);
