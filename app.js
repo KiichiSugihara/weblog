03d928e43021c191dbd0586a2d6756c6e46507e4
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2019-12-02 22:44:52 +0900

    add views index

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log(process.env.NODE_ENV);
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+app.disable('x-powered-by');
+
+app.use('/public', express.static(__dirname + '/public'));
+app.use('/', require('./routes/index.js'));
+
+app.listen(3000);
