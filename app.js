d32e3d56381e5e02c4cc14aae92c86b4ca5d7695
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2019-12-19 19:40:55 +0900

    add (logging): add accessLog

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var accesslogger = require('./lib/log/accesslogger.js');
 var systemlogger = require('./lib/log/systemlogger.js');
 var express = require('express');
 var app = express();
@@ -13,6 +14,9 @@ app.use(
       (process.env.NODE_ENV === 'development' ? 'development' : 'production')
   )
 );
+
+app.use(accesslogger());
+
 app.use('/', require('./routes/index.js'));
 
 app.use(systemlogger());
